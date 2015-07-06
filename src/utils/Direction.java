5f681625b763c36d12467f2f25c76aaa81e74ce1
Asakha <clement.marcandella@viacesi.fr>
2015-07-06 15:09:11 +0200

    Complete the enumeration of directions available.

--- src/utils/Direction.java
+++ src/utils/Direction.java
@@ -1,5 +1,10 @@
 package utils;
-
+/*
+ * This enumeration is used to set directions of our snake in game (Then North, South, East and West)
+ */
 public enum Direction {
-
+	NORTH,
+	SOUTH,
+	EAST,
+	WEST
 }
