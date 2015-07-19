2f18594fbe325dcdbfc257a116668b204fdcfb53
Asakha <clement.marcandella@viacesi.fr>
2015-07-19 17:26:43 +0200

    Creation of the map class.

---
+++ src/engine/Map.java
@@ -0,0 +1,12 @@
+package engine;
+/*
+ * This class is not used for the moment.
+ * We will add this class later.
+ * 
+ * This class is the map of the game.
+ * 
+ */
+public class Map {
+	int mapTab[][] = new int[10][10];
+	
+}
